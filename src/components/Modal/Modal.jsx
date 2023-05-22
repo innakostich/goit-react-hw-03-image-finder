28890310a4ba09411f408bb043bd4683e2a317c6
Inna Kostich <114675831+innakostich@users.noreply.github.com>
2023-05-22 11:00:50 +0200

    adds logic correction

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,4 +1,52 @@
 
+// import React, { Component } from 'react';
+// import PropTypes from 'prop-types';
+// import css from './Modal.module.css';
+
+// class Modal extends Component {
+//   componentDidMount() {
+//     document.addEventListener('keydown', this.handleKeyDown);
+//   }
+
+//   componentWillUnmount() {
+//     document.removeEventListener('keydown', this.handleKeyDown);
+//   }
+
+//   handleKeyDown = (e) => {
+//     if (e.key === 'Escape') {
+//       this.props.onClose();
+//     }
+//   };
+
+//   handleClick = (e) => {
+//     if (e.target === e.currentTarget) {
+//       this.props.onClose();
+//     }
+//   };
+
+//   render() {
+//     const { isOpen, image } = this.props;
+
+//     return (
+//       <div
+//         className={isOpen ? css.overlay : css.hidden}
+//         onClick={this.handleClick}
+//       >
+//         <div className={css.modal}>
+//           <img src={image} alt="" />
+//         </div>
+//       </div>
+//     );
+//   }
+// }
+
+// Modal.propTypes = {
+//   isOpen: PropTypes.bool.isRequired,
+//   image: PropTypes.string.isRequired,
+//   onClose: PropTypes.func.isRequired,
+// };
+
+// export default Modal;
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import css from './Modal.module.css';
--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,4 +1,64 @@
 
+// import React, { Component } from 'react';
+// import PropTypes from 'prop-types';
+// import { toast } from 'react-toastify';
+// import css from './Searchbar.module.css';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { faSearch } from '@fortawesome/free-solid-svg-icons';
+
+// class Searchbar extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       searchQuery: '',
+//     };
+//   }
+
+//   handleSubmit = (e) => {
+//     e.preventDefault();
+//     const { searchQuery } = this.state;
+
+//     if (searchQuery.trim() === '') {
+//       toast.error('Please enter a search query');
+//       return;
+//     }
+
+//     this.props.onSubmit(searchQuery);
+//     this.setState({ searchQuery: '' });
+//   };
+
+//   handleChange = (e) => {
+//     this.setState({ searchQuery: e.target.value });
+//   };
+
+//   render() {
+//     return (
+//       <header className={css.searchbar}>
+//         <form className={css.form} onSubmit={this.handleSubmit}>
+//           <button type="submit" className={css.button}>
+//             <FontAwesomeIcon icon={faSearch} />
+//           </button>
+
+//           <input
+//             className={css.input}
+//             type="text"
+//             autoComplete="off"
+//             autoFocus
+//             placeholder="Search images and photos"
+//             value={this.state.searchQuery}
+//             onChange={this.handleChange}
+//           />
+//         </form>
+//       </header>
+//     );
+//   }
+// }
+
+// Searchbar.propTypes = {
+//   onSubmit: PropTypes.func.isRequired,
+// };
+
+// export default Searchbar;
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { toast } from 'react-toastify';
